4bec4ce4df7c87a0be3e90a444df1ed0a6a8e0c5
mokshada335 <96331579+mokshada335@users.noreply.github.com>
2024-11-27 17:08:50 +0530

    Update food_classification.py

--- food_classification.py
+++ food_classification.py
@@ -1,10 +1,2 @@
-{
-    "someOtherKey": "value",
-    "dockerImageVersionId": 30616,
-    "isInternetEnabled": True,  # Fix here
-    "language": "python",
-    "sourceType": "notebook",
-    "isGpuEnabled": True
-}
 
 
